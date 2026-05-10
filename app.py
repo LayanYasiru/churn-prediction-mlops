8c4604bcfccf27ba3953a4290870636dceb23b9f
LayanYasiru <yasirulayan4@gmail.com>
2026-05-10 17:23:18 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,72 @@
+import os
+import sys
+import uvicorn
+import pandas as pd
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+
+# Project paths
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(ROOT_DIR, 'src'))
+from model_inference import ModelInference
+
+app = FastAPI(title="Churn Prediction API", version="1.0.0")
+
+# Inference System එක load කිරීම
+inference_system = ModelInference(model_path='artifacts/models/churn_analysis.joblib')
+
+class CustomerData(BaseModel):
+    RowNumber: int = Field(..., example=1)
+    CustomerId: int = Field(..., example=15634602)
+    Firstname: str = Field(..., example="Yasiru")
+    Lastname: str = Field(..., example="Liyanage")
+    CreditScore: int = Field(..., example=619)
+    Geography: str = Field(..., example="France") # France, Germany, Spain
+    Gender: str = Field(..., example="Female")    # Female, Male
+    Age: int = Field(..., example=42)
+    Tenure: int = Field(..., example=2)
+    Balance: float = Field(..., example=0.0)
+    NumOfProducts: int = Field(..., example=1)
+    HasCrCard: int = Field(..., example=1)
+    IsActiveMember: int = Field(..., example=1)
+    EstimatedSalary: float = Field(..., example=101348.88)
+
+@app.get("/")
+def home():
+    return {"message": "Churn Prediction API is Online. Go to /docs"}
+
+@app.post("/predict")
+async def predict(data: CustomerData):
+    try:
+        # 1. දත්ත dictionary එකක් ලෙස ලබා ගැනීම
+        input_data = data.model_dump()
+        
+        # 2. MANUAL ONE-HOT ENCODING (Model එක බලාපොරොත්තු වන columns සෑදීම)
+        # Gender Encoding
+        input_data['Gender_Female'] = 1 if input_data['Gender'] == 'Female' else 0
+        input_data['Gender_Male'] = 1 if input_data['Gender'] == 'Male' else 0
+
+        # Geography Encoding
+        input_data['Geography_France'] = 1 if input_data['Geography'] == 'France' else 0
+        input_data['Geography_Germany'] = 1 if input_data['Geography'] == 'Germany' else 0
+        input_data['Geography_Spain'] = 1 if input_data['Geography'] == 'Spain' else 0
+
+        # 3. Prediction එක ලබා ගැනීම
+        # මෙහිදී 'inference_system.predict' තුළ අභ්‍යන්තරව Scaling සහ Binning සිදු වේ
+        result = inference_system.predict(input_data)
+
+        return {
+            "Gender Female":input_data['Gender_Female'],
+            "Gender Male":input_data['Gender_Female']  ,
+            "customer_id": data.CustomerId,
+            "prediction": result['Status'],
+            "confidence": result['Confidence']
+        }
+
+    except Exception as e:
+        # Error එක log එකේ බලාගන්න පහත පේළිය පාවිච්චි කරන්න
+        print(f"PREDICTION ERROR: {str(e)}")
+        raise HTTPException(status_code=500, detail=str(e))
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
